c8be0a2e885cc8807da71e9658d9ad1e5ae6f0e9
Abdishakur Abdalla Gesey <karbuuno@gmail.com>
2023-04-18 13:20:17 +0100

    added pictures in README

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Note Taker application
 
-\*\* An application that can be used to write, save, and delete notes. This application uses an express backend and save and retrieve note data from a JSON file.
+An application that can be used to write, save, and delete notes. This application uses an express backend and save and retrieve note data from a JSON file. <br>
+[deployed link](https://note-taker-nodjs.herokuapp.com/)<br>
+
+[github link](https://github.com/Karbuuno/note-taker-app)
+
+![alt first page image](/public/assets/images/image-1.png)
+![alt first page image](/public/assets/images/image-2.png)
+![alt first page image](/public/assets/images/image-3.png)
+![alt first page image](/public/assets/images/image-4.png)
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// all dependencies
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
@@ -14,7 +15,7 @@ app.use(express.json());
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-
+// getting data json json file
 app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/db/db.json"));
 });
@@ -22,14 +23,14 @@ app.get("/api/notes", (req, res) => {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-
+// creating data using post request
 app.post("/api/notes", (req, res) => {
   let newNote = {
     id: uuid(),
     title: req.body.title,
     text: req.body.text,
   };
-
+  // reading dta form json file
   fs.readFile("./db/db.json", (err, data) => {
     if (err) throw err;
 
@@ -44,17 +45,17 @@ app.post("/api/notes", (req, res) => {
     });
   });
 });
-
+// deleting data
 app.delete("/api/notes/:id", (req, res) => {
-  let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
   let id = req.params.id.toString();
 
-  noteList = noteList.filter(selected => {
-    return selected.id != id;
+  deletedNote = notes.filter(note => {
+    return note.id != id;
   });
 
-  fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
-  res.json(noteList);
+  fs.writeFileSync("./db/db.json", JSON.stringify(deletedNote));
+  res.json(deletedNote);
 });
 
 //listen tot he port when deployed
